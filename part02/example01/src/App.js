4f96e00962779ba95355ca83d59cbd5d2fb65a0a
kobong8 <fblsj90@gmail.com>
2025-03-23 11:33:05 +0900

    helper update

--- part02/example01/src/App.js
+++ part02/example01/src/App.js
@@ -1,7 +1,7 @@
 import { useHelper } from "@react-three/drei"
 import { Canvas, useLoader } from "@react-three/fiber"
 import { useRef } from "react"
-import { DirectionalLight, DirectionalLightHelper } from "three"
+import { DirectionalLightHelper } from "three"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
 
 function Box(props) {
@@ -9,15 +9,15 @@ function Box(props) {
     <mesh {...props}>
       <boxGeometry args={[1]}/>
       {/* <meshBasicMaterial color={"hotpink"}/> */}
-      <meshStandardMaterial color={"hotpink"}/>
+      <meshStandardMaterial color={"hotpink"} wireframe/>
 
     </mesh>
   )
 }
 
 function Model(){
-  const glb = useLoader( GLTFLoader, "/models/earth.glb" )
-  console.log(glb)
+  const glb = useLoader( GLTFLoader, "/models/earth.glb" );
+  console.log(glb);
   return(
     <></>
   )
@@ -25,19 +25,19 @@ function Model(){
 
 function Lights(){
   const ref = useRef();
-  useHelper(ref, DirectionalLightHelper, 10, "red")
+  useHelper(ref, DirectionalLightHelper, 1, "red");
   return(
-    <directionalLight position={[1, 3, -1]} intensity={3}/>
+    <directionalLight ref={ref} position={[1, 3, -1]} intensity={3}/>
   )
 }
 
 function App() {
-  console.log("Canvas")
   return (
     <Canvas camera={{position: [0, 1, 2]}}>
         <color attach="background" args={["yellow"]}></color>
         <Lights/>
         <Box rotation-y={1}/>
+        <Box position={[0, 0, -1]} rotation-y={1}/>
     </Canvas>
   );
 }
