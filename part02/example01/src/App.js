a3df3c1889b1335eda2c310da1bfeff6a55a434f
kobong8 <fblsj90@gmail.com>
2025-03-23 11:16:27 +0900

    App update

--- part02/example01/src/App.js
+++ part02/example01/src/App.js
@@ -1,6 +1,30 @@
+import { Canvas, useLoader } from "@react-three/fiber"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+
+function Model(){
+  const glb = useLoader( GLTFLoader, "/models/earth.glb" )
+  console.log(glb)
+  return(
+    <></>
+  )
+}
+
+function Box() {
+  return(
+    <mesh>
+      <boxGeometry args={[1]}/>
+      <meshBasicMaterial color={"hotpink"}/>
+    </mesh>
+  )
+}
+
 function App() {
+  console.log("Canvas")
   return (
-    <div className="h1">Hello R3F</div>
+    <Canvas>
+        <color attach="background" args={["yellow"]}></color>
+        <Model/>
+    </Canvas>
   );
 }
 
