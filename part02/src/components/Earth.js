1abc08c88c9a70305ddc9ff01142feacff33f8ce
kobong8 <fblsj90@gmail.com>
2025-03-23 17:30:06 +0900

    rotate Earth

--- part02/src/components/Earth.js
+++ part02/src/components/Earth.js
@@ -1,10 +1,18 @@
-import { useLoader } from "@react-three/fiber"
+import { useFrame, useLoader } from "@react-three/fiber"
+import { useRef } from "react"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-export function Earth(props){
+export function Earth(){
   const glb = useLoader( GLTFLoader, "/models/earth.glb" );
+
+  const ref = useRef(null)
+
+  useFrame((state, delta) => {
+    ref.current.rotation.y += delta * 0.1
+  })
+
   return(
-    <mesh {...props}>
+    <mesh ref={ref} scale={1.3} rotation-y={Math.PI/2} position={[0, -2, 0]}>
       <primitive object={glb.scene}/>
     </mesh>
   )
--- part02/src/components/Scene.js
+++ part02/src/components/Scene.js
@@ -8,15 +8,15 @@ export function Scene() {
     <Canvas camera={{position: [0, 1, 5]}}>
         <color attach="background" args={["rgb(67, 127, 240) 100%"]}></color>
         <Lights/>
-        <Earth position={[0, -2, 0]}/>
-        <Weather position={[-2, 0, 0]} weather={"clear"}/>
+        <Earth/>
+        {/* <Weather position={[-2, 0, 0]} weather={"clear"}/>
         <Weather position={[-0.75, 0, 0]} weather={"cloud"}/>
         <Weather position={[0.75, 0, 0]} weather={"clouds"}/>
         <Weather position={[2, 0, 0]} weather={"mist"}/>
 
         <Weather position={[-1.5, 1, 0]} weather={"rain2"}/>
         <Weather position={[0, 1, 0]} weather={"snow"}/>
-        <Weather position={[1.5, 1, 0]} weather={"rain"}/>
+        <Weather position={[1.5, 1, 0]} weather={"rain"}/> */}
     </Canvas>
     ]
 }
