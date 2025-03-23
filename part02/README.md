835acfedcbcdf97186d9deec817879a41eb6d904
kobong8 <fblsj90@gmail.com>
2025-03-23 17:20:14 +0900

    Weather update

--- part02/README.md
+++ part02/README.md
@@ -4,7 +4,9 @@
 npm i
 ```
 
-1. [TODO] Warning: Each child in a list should have a unique "key" prop. 
+1. [TODO] props의 역할
+
+2. [TODO] Warning: Each child in a list should have a unique "key" prop. 
 
 ```
 Warning: Each child in a list should have a unique "key" prop. See https://reactjs.org/link/warning-keys for more information.
--- part02/src/components/Scene.js
+++ part02/src/components/Scene.js
@@ -1,6 +1,7 @@
 import { Canvas } from "@react-three/fiber"
 import { Lights } from "./Lights"
 import { Earth } from "./Earth"
+import { Weather } from "./Weather"
 
 export function Scene() {
     return[
@@ -8,6 +9,14 @@ export function Scene() {
         <color attach="background" args={["rgb(67, 127, 240) 100%"]}></color>
         <Lights/>
         <Earth position={[0, -2, 0]}/>
+        <Weather position={[-2, 0, 0]} weather={"clear"}/>
+        <Weather position={[-0.75, 0, 0]} weather={"cloud"}/>
+        <Weather position={[0.75, 0, 0]} weather={"clouds"}/>
+        <Weather position={[2, 0, 0]} weather={"mist"}/>
+
+        <Weather position={[-1.5, 1, 0]} weather={"rain2"}/>
+        <Weather position={[0, 1, 0]} weather={"snow"}/>
+        <Weather position={[1.5, 1, 0]} weather={"rain"}/>
     </Canvas>
     ]
 }
